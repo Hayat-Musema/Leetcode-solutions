c516eb1176af913da084b4bae3f5d7298be53b7e
Hayat-Musema <hayat11.mf@gmail.com>
2025-10-15 14:24:46 -0700

    Time: 453 ms (5.09%), Space: 93.6 MB (5.26%) - LeetHub

---
+++ 2418-sort-the-people/2418-sort-the-people.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+    for (int i = 0; i < heights.size() - 1; i++) {
+        for (int j = i + 1; j < heights.size(); j++) {
+            if (heights[i] < heights[j]) { 
+             
+                int temp = heights[i];
+                heights[i] = heights[j];
+                heights[j] = temp;
+
+            
+                string temp2 = names[i];
+                names[i] = names[j];
+                names[j] = temp2;
+            }
+        }
+    }
+    return names;
+}
+};
