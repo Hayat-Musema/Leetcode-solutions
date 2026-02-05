18bc147e750aeb904dfe15a1311df606ad3c6440
Hayat Musema <hayat11.mf@gmail.com>
2026-02-05 12:01:48 -0800

    Time: 4 ms (80.11%), Space: 25.7 MB (40.33%) - LeetHub

---
+++ 3379-transformed-array/3379-transformed-array.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> constructTransformedArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> result(n);
+
+        for (int i = 0; i < n; ++i) {
+            // Compute circular index using modulo for wrap‑around
+            int target = ((i + nums[i]) % n + n) % n;
+            result[i] = nums[target];
+        }
+
+        return result;
+    }
+};
