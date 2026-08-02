5182517d782f7db278dea759a9c271e7e48c9f18
Hayat Musema <hayat11.mf@gmail.com>
2026-08-02 23:47:11 +0300

    Time: 0 ms (100%), Space: 10.4 MB (68.89%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        // Alice can always guarantee a win by choosing all odd or all even indexed piles.
+        return true;
+    }
+};
